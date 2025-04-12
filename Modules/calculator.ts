fad8b0185e9df06d6a2441daa432a1034494d08c
Daniel Lorenzo <daniel.lorenzo.rodriguez98@gmail.com>
2025-04-12 19:15:03 +0200

    Modules

---
+++ Modules/calculator.ts
@@ -0,0 +1,17 @@
+export function suma(num1: number, num2: number): number {
+    return num1 + num2;
+}
+
+export function resta(num1: number, num2: number): number {
+    return num1 - num2;
+}
+
+export function multiplicacion(num1: number, num2: number):number {
+    return num1 * num2;
+}
+
+export function division(num1: number, num2: number):number {
+    return num1 / num2;
+}
+
+export const Pi = 3.1416;
---
+++ Modules/main.ts
@@ -0,0 +1,5 @@
+import { suma, resta, multiplicacion, division } from "./calculator";
+import { Pi } from "./calculator";
+
+console.log(suma(10, 45));
+console.log(resta(Pi, 2));
